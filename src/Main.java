838426d614d3038892aa9f097440ad367210434b
chika <152640307+chika1605@users.noreply.github.com>
2025-02-11 16:02:54 +0600

    Калькулятор по вычеслению факториала

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello GrowthHungry!!!");
+        // Создаем объект Scanner для считывания ввода
+        Scanner scanner = new Scanner(System.in);
+
+        // Запрашиваем у пользователя ввод числа
+        System.out.print("Введите число для вычисления факториала: ");
+        int number = scanner.nextInt();
+
+        // Проверяем, что число неотрицательное
+        if (number < 0) {
+            System.out.println("Факториал можно вычислить только для неотрицательных чисел.");
+        } else {
+            // Вычисляем факториал
+            long factorial = 1;
+            for (int i = 1; i <= number; i++) {
+                factorial *= i;
+            }
+            // Выводим результат
+            System.out.println("Факториал числа " + number + " равен: " + factorial);
+        }
+
+        // Закрываем сканер
+        scanner.close();
     }
 }
