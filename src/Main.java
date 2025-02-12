f0a35652c58d4a1c0ffff67287432ff36b6ba7d4
chika <152640307+chika1605@users.noreply.github.com>
2025-02-12 20:47:12 +0600

    Калькулятор

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,49 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем объект Scanner для считывания ввода
         Scanner scanner = new Scanner(System.in);
 
-        // Запрашиваем у пользователя ввод числа
-        System.out.print("Введите число для вычисления факториала: ");
-        int number = scanner.nextInt();
+        // Запрос у пользователя первого числа
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
 
-        // Проверяем, что число неотрицательное
-        if (number < 0) {
-            System.out.println("Факториал можно вычислить только для неотрицательных чисел.");
-        } else {
-            // Вычисляем факториал
-            long factorial = 1;
-            for (int i = 1; i <= number; i++) {
-                factorial *= i;
-            }
-            // Выводим результат
-            System.out.println("Факториал числа " + number + " равен: " + factorial);
+        // Запрос у пользователя операции
+        System.out.print("Выберите операцию (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        // Запрос у пользователя второго числа
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        // Выполнение выбранной операции
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Ошибка: деление на ноль!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Ошибка: некорректный оператор!");
+                return;
         }
 
-        // Закрываем сканер
+        // Вывод результата
+        System.out.println("Результат: " + result);
+
         scanner.close();
     }
 }
