4948da973a377751eca9a10a9e0e5a30aa6a7e1b
chika <152640307+chika1605@users.noreply.github.com>
2025-02-16 22:57:09 +0600

    Калькулятор

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,7 @@ public class Main {
                 }
                 break;
             default:
-                System.out.println("Ошибка: некорректный!");
+                System.out.println("Ошибка: некорреный!");
                 return;
         }
 
